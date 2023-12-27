b2f284e6f76ab70871e1f538fc6391339dba792c
Daniel Baigel <dbaigel@yext.com>
2023-12-27 15:02:03 -0500

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import ReportCard from "./ReportCard";
 
 const App = () => {
   return (
-    <div className="flex justify-center px-4 py-6">
-      <div className="w-full max-w-5xl">
-        <SearchBar />
-        <VerticalResults CardComponent={ReportCard} />
+    <div>
+      <h1 className="font-bold text-4xl bg-black text-center py-4 text-white">
+        <span className="text-blue-200">Ice</span>berg Reports
+      </h1>
+
+      <div className="flex justify-center px-4 py-6">
+        <div className="w-full max-w-5xl">
+          <SearchBar />
+          <VerticalResults CardComponent={ReportCard} />
+        </div>
       </div>
     </div>
   );
